b156d0338dc6d631918de87018f592f2d9d29693
Thomas Tursics <thomas@tursics.de>
2022-05-05 00:12:48 +0200

    add own data service

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Based on https://github.com/opendata-guru/peacock-user-ui
 Github Pages: https://tursics.github.io/schule-datenportal/
 
 Ideation: https://codepen.io/nickmoreton/pen/bEMaoE
+
+Migrate Google API: https://developers.google.com/sheets/api/guides/migration#v4-api_9
--- js/custom.js
+++ js/custom.js
@@ -23,3 +23,52 @@ CONFIG_APP_ROUTER_ROUTE_2_NAME = 'Portal';
 CONFIG_APP_ROUTER_ROUTE_2_PATH = '/list';
 CONFIG_APP_ROUTER_ROUTE_2_COMPONENT = 'Datasets';
 CONFIG_APP_ROUTER_ROUTE_2_REQUIRES_AUTH = false;
+
+class GoogleSpreadsheetDataService {
+    constructor(baseUrl) {
+        // Clear newURL
+//        $scope.newURL = '';
+
+        // Empty array for all the original grid IDs
+//        $scope.oldGrids = [];
+        // Empty array for all the new grid IDs
+//        $scope.newGrids = [];
+
+        var preString = baseUrl.substring(baseUrl.indexOf('/d/') + 3);
+        if (preString.indexOf('/') > -1) {
+            this.spreadsheetId = preString.substring(0, preString.indexOf('/'));
+        } else {
+            this.spreadsheetId = preString;
+        }
+
+        this.grid = baseUrl.substring(baseUrl.indexOf('=') + 1);
+
+        this.uriXMLV3 = 'https://spreadsheets.google.com/feeds/worksheets/' + this.spreadsheetId + '/public/full';
+//        this.uriXML = 'https://www.googleapis.com/auth/spreadsheets.readonly/' + this.spreadsheetId;
+        this.uriXML = 'https://sheets.googleapis.com/v4/spreadsheets/' + this.spreadsheetId;
+
+        console.log('spreadsheet id ' + this.spreadsheetId);
+        console.log('grid ' + this.grid);
+        console.log('uriXML v3 ' + this.uriXMLV3);
+        console.log('uriXML v4 ' + this.uriXML);
+        this.baseUrl = baseUrl;
+    }
+};
+
+CONFIG_APP_DATA_SERVICE = {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl;
+    },
+
+    getSingle(id) {
+        console.log('getSingle');
+    },
+
+    get(q, facets, limit, page = 0 /* , sort = 'relevance+asc, last_modified+asc, name+asc', facetOperator = "AND", facetGroupOperator = "AND", geoBounds */) {
+        console.log('get');
+    }
+};
+
+CONFIG_APP_DATA_URL = 'https://docs.google.com/spreadsheets/d/1BEDAUycmxo2ekI3FeLinsUqbqQniKXpqKR067INtsos/edit#gid=0';
+//CONFIG_APP_DATA_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSaLGv04zGBi7TnqZn6DJS9vb_6ynVPD0ShDqv57uyRTLgr7Nknbx7344_wtORc_i3ItZQRzDK9GXrV/pub?gid=0&single=true&output=csv';
+CONFIG_APP_DATA_SERVICE = GoogleSpreadsheetDataService;
